bb1f71dd202ac79ed2036bf090e134bc2a7fae5d
sas8254 <shashibhushansingh11@gmail.com>
2024-02-01 12:11:54 +0530

    add auto tracking server

--- app.js
+++ app.js
@@ -47,15 +47,34 @@ app.get("/", (req, res) => {
   res.send(`got the request no ${count}`);
 });
 
+const autoTracker = async () => {
+  try {
+    const response = await axios.get(
+      "https://ma-jayveer.onrender.com/users/get-user-count"
+    );
+    console.log(`Status: ${response.status}`);
+    console.log("Body: ", response.data, " form autoTracker server");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.get("/", (req, res) => {
+  count++;
+  res.send(`got the request no ${count}`);
+});
+
 // Send the first request when the app starts
 menualServer();
 kevalServer();
 jayveerServer();
+autoTracker();
 
 // Then send a request every 10 minutes
 setInterval(menualServer, 600000);
 setInterval(kevalServer, 590000);
 setInterval(jayveerServer, 610000);
+setInterval(autoTracker, 620000);
 // setInterval(printTime, 2000);
 
 const port = process.env.PORT || 3005;
